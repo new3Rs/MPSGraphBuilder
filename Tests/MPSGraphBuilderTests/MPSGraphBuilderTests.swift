c106475396d377b42e861f20d9dc6695fd1f8082
y-ich <ichikawa.yuji@gmail.com>
2023-10-23 14:00:58 +0900

    performance measure

--- Tests/MPSGraphBuilderTests/MPSGraphBuilderTests.swift
+++ Tests/MPSGraphBuilderTests/MPSGraphBuilderTests.swift
@@ -10,4 +10,22 @@ final class MPSGraphBuilderTests: XCTestCase {
     func testKataGo() throws {
         XCTAssertNoThrow(try mlmodelToMPSGraph(from: Bundle.module.url(forResource: "MLModels/KataGoKata1_b18c384nbts658", withExtension: "mlmodel")!))
     }
+
+    func testPerformanceKataGo() throws {
+        let batch = NSNumber(value: 16)
+        let device = MTLCreateSystemDefaultDevice()!
+        let (userDefined, inputs, outputs, graph) = try mlmodelToMPSGraph(from: Bundle.module.url(forResource: "MLModels/KataGoKata1_b18c384nbts658", withExtension: "mlmodel")!)
+        let feeds = [
+            inputs["bin_inputs"]!: MPSGraphTensorData(MPSNDArray(device: device, descriptor: MPSNDArrayDescriptor(dataType: .float32, shape: [batch,22,19,19]))),
+            inputs["global_inputs"]!: MPSGraphTensorData(MPSNDArray(device: device, descriptor: MPSNDArrayDescriptor(dataType: .float32, shape: [batch,19]))),
+            inputs["mask_sum_hw_sqrt_offset_10"]!: MPSGraphTensorData(MPSNDArray(device: device, descriptor: MPSNDArrayDescriptor(dataType: .float32, shape: [1,1,1,1])))
+        ]
+        measure {
+            let _ = graph.run(
+                feeds: feeds,
+                targetTensors: outputs.map { k, v in v },
+                targetOperations: nil
+            )
+        }
+    }
 }
